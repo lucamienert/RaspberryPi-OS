cc1fa70f5dcb90bcf1000badbaa27617f3232c87
Luca Mienert <67235946+lucamienert@users.noreply.github.com>
2022-05-19 15:01:16 +0200

    Creating stdarg

---
+++ include/stdarg.h
@@ -0,0 +1,12 @@
+#ifndef RASPOS_STDARG_H_
+#define RASPOS_STDARG_H_
+
+#pragma once
+
+typedef char *va_list;
+#define va_start(ap,parmn) (void)((ap) = (char*)(&(parmn) + 1))
+#define va_end(ap) (void)((ap) = 0)
+#define va_arg(ap, type) \
+    (((type*)((ap) = ((ap) + sizeof(type))))[-1])
+    
+#endif
